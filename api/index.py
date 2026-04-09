623f73d9966c19735ac9aae15568cfc4dc54dc2b
Phuoc <33310761+phuochq@users.noreply.github.com>
2026-04-09 14:14:32 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -22,6 +22,8 @@ ydl_opts = {
     'noplaylist': True,
     'format': 'best',
     'source_address': '0.0.0.0',
+    'nocheckcertificate': True,
+    'geo_bypass': True,
     # Giả lập iPhone để Jimeng không chặn
 'http_headers': {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
